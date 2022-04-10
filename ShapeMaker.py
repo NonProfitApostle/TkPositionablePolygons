2eeda3df98feff206c0f83b31e9416b219712a37
NonProfitApostle <93163420+NonProfitApostle@users.noreply.github.com>
2022-04-09 22:56:36 -0700

    root window

---
+++ ShapeMaker.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from canvtest  import ShapeCanvas as sc
+
+
+
+class ShapeMaker(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.canv = sc(self, width=800, height=600)
+        self.canv.grid(row= 1, column = 2, sticky = 'nsew')
+
+    def run(self):
+        self.mainloop()  
+
+
+    def Exit(self):
+        self.quit()
+
+
+if __name__ == "__main__":
+    sf = ShapeMaker()
+    sf.run()
